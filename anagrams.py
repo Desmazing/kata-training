6cf3bef69f378c06d99e12008fe37974273b62ad
Desmazing <dkimodera1@gmail.com>
2022-09-02 12:00:40 +0300

    find anagrams

---
+++ anagrams.py
@@ -0,0 +1,20 @@
+# return a list of anagrams given a word and an array of words\
+# for instance anagrams('abba', ['bbaa', 'aabb', 'abcd', 'efgh', 'abdf'])
+# the above case returns ['bbaa', 'aabb']
+
+def anagrams(word, words):
+    """this is a more comprehensive solution to the problem."""
+    return [term for term in words if sorted(term) == sorted(word)]
+
+
+def anagrams2(word, words):
+    """this is a stepwise solution"""
+    ana_list = []
+    for term in words:
+        if sorted(term) == sorted(word):
+            ana_list.append(term)
+    return ana_list
+
+
+test1 = print(anagrams('abba', ['bbaa', 'aabb', 'abcd', 'efgh', 'abdf']))
+test2 = print(anagrams2('hello', ['lo', 'ehllo', 'lohel', 'hello', 'hel']))
