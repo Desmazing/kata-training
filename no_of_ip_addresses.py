7971db00e07b8bea77824bd6d6dcc433e8529a51
Desmazing <dkimodera1@gmail.com>
2022-09-03 14:40:51 +0300

    finding no of ip addresses

--- no_of_ip_addresses.py
+++ no_of_ip_addresses.py
@@ -2,7 +2,10 @@
 # returns the no. of ip addresses between them excluding the last one
 # the latter is always larger than the former
 
+
 def no_of_ips(ip1, ip2):
+    """this version uses manual calculations in each section of the ipv4 
+    to compute total no of ip addresses"""
     ip1, ip2 = ip1.split('.'), ip2.split('.')
     total_1 = (int(ip1[0]) * 16777216) + (int(ip1[1]) * 65536) + (int(ip1[2]) * 256) + (int(ip1[3]) * 1)
     total_2 = (int(ip2[0]) * 16777216) + (int(ip2[1]) * 65536) + (int(ip2[2]) * 256) + (int(ip2[3]) * 1)
