ba1be138805610341e66fa3635e95a7939a7da75
Desmazing <dkimodera1@gmail.com>
2022-09-09 15:04:57 +0300

    expanded number format

--- expanded_numbers.py
+++ expanded_numbers.py
@@ -1,11 +1,9 @@
 def expanded_form(num):
     res = []
     final = []
-    a = list(str(num)[::-1])
-    for i in a:
-        res.append(int(i))
+    res = list((str(num)[::-1]))
     for j in range(0, len(res)):
-        res[j] = res[j] * (10 ** j)
+        res[j] = int(res[j]) * (10 ** j)
         if res[j] != 0:
             final.append(str(res[j]))
     return ' + '.join(reversed(final))
