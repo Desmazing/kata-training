7148662950a930c69b888ee42933019ce61e49bc
Desmazing <dkimodera1@gmail.com>
2022-09-09 15:13:41 +0300

    expanded number format

--- expanded_numbers.py
+++ expanded_numbers.py
@@ -1,5 +1,4 @@
 def expanded_form(num):
-    res = []
     final = []
     res = list((str(num)[::-1]))
     for j in range(0, len(res)):
@@ -9,4 +8,14 @@ def expanded_form(num):
     return ' + '.join(reversed(final))
 
 
+def expanded_form2(num):
+    final = []
+    res = list((str(num)[::-1]))
+    for i,j in enumerate(res):
+        if j != '0':
+            final.append(str(int(j) * (10 ** i)))
+    return ' + '.join(reversed(final))
+
+
 test1 = print(expanded_form(4982042))
+test2 = print(expanded_form2(209))
