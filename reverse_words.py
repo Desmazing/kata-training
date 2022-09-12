41544b34df08bf5b7030485b1e069daacbeb09f4
Desmazing <dkimodera1@gmail.com>
2022-09-12 11:09:57 +0300

    reverse words in a string

---
+++ reverse_words.py
@@ -0,0 +1,11 @@
+def reverse_words(text):
+    a = text.split()
+    b = []
+    for i in a:
+        b.append(i[::-1])
+    return ' '.join(b)
+
+
+test1 = print(reverse_words('The quick brown fox jumps over the lazy dog.'))
+test2 = print(reverse_words('apple'))
+test3 = print(reverse_words('a b c d')) # should return the same string
