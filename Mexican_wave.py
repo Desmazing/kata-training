edd473475e2daa4ba74336a5eaeff7fb78dafa2d
Desmazing <dkimodera1@gmail.com>
2022-09-12 12:24:47 +0300

    The Mexican wave

---
+++ Mexican_wave.py
@@ -0,0 +1,9 @@
+# this is an implementation of a Mexican wave
+# the strings represent a sequence of people in the crowd
+# white space character refers to an empty seat in the stands
+
+
+def wave(people):
+    """to be continued"""
+
+test1 = print(wave('hello')) # should return ['Hello', 'hEllo','heLlo','helLo','hellO']
