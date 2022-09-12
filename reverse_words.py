3d761eaabcf739e4db2a9872c6ad42994a7787f5
Desmazing <dkimodera1@gmail.com>
2022-09-12 11:26:04 +0300

    reversed words in a text

--- reverse_words.py
+++ reverse_words.py
@@ -7,6 +7,17 @@ def reverse_words(text):
     return ' '.join(b)
 
 
+def reverse_words2(text):
+    """this second solution works for double spaced words
+    the extra inclusion is the delimitor in the string.split() method"""
+    a = text.split(' ')
+    b = []
+    for i in a:
+        b.append(i[::-1])
+    return ' '.join(b)
+
+
 test1 = print(reverse_words('The quick brown fox jumps over the lazy dog.'))
 test2 = print(reverse_words('apple'))
 test3 = print(reverse_words('a b c d')) # should return the same string
+test4 = print(reverse_words2('double  spaced  words'))
