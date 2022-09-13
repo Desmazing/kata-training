e03905e627621aa89cff68bea630f1559ade90aa
Desmazing <dkimodera1@gmail.com>
2022-09-13 20:59:20 +0300

    sum of a mix of data types in array

---
+++ map_function.py
@@ -0,0 +1,15 @@
+# exploring using the map() function
+# the map function returns a map object(iterator) of results after applying function to each item in iterable
+
+
+def sum_mix(arr):
+    """return the sum of the mixed data types in the array
+    types are integer and string"""
+    total = 0
+    for i in arr:
+         total+= int(i)
+    return total
+
+
+test1 = print(sum_mix([1,'2']))
+test2 = print(sum_mix([1,2,4,'4','3',6, '7']))
