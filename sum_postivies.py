ee9c87a7039955ead8bd2238c0c16301d6473508
Desmazing <dkimodera1@gmail.com>
2022-09-13 11:53:41 +0300

    sum of pos, array of neg

---
+++ sum_postivies.py
@@ -0,0 +1,15 @@
+# this requires us to return the sum of all positive numbers in an array
+# and an array of all the negative numbers
+
+
+def array_sort(arr):
+    neg = []
+    pos = 0
+    for i in arr:
+        if i < 0: neg.append(i)
+        elif i > 0: pos += 1
+    return [neg, pos]
+
+
+test1 = print(array_sort([-1,2,1,3]))
+test2 = print(array_sort([]))
