14d21fafea569b47a9f5a92c241497b5ff4593dd
Desmazing <dkimodera1@gmail.com>
2022-09-15 19:29:04 +0300

    validate pin

--- validate_pin.py
+++ validate_pin.py
@@ -1,5 +1,6 @@
 import string
 
+
 def validate_pin(pin):
     if len(pin) != 4 and len(pin) != 6: return False
     for i in pin:
@@ -8,9 +9,14 @@ def validate_pin(pin):
     return True
 
 
+def validate_pin2(pin):
+    """this is a refactored version"""
+    return len(pin) in (4, 6) and pin.isdigit()
+
+
 test1 = print(validate_pin('1234'))
 test2 = print(validate_pin("-1234"))
 test3 = print(validate_pin("1.234"))
-test4 = print(validate_pin("00000000"))
+test4 = print(validate_pin2("00000000"))
 test5 = print(validate_pin("a234"))
 test6 = print(validate_pin("124234"))
