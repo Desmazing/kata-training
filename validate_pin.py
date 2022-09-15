2fca810fea118f173e7bc04f0d6e5a3dadd59244
Desmazing <dkimodera1@gmail.com>
2022-09-15 19:17:12 +0300

    validate pin

---
+++ validate_pin.py
@@ -0,0 +1,9 @@
+def validate_pin(pin):
+    pass
+
+
+test1 = print(validate_pin('1234'))
+test2 = print(validate_pin("-1234"))
+test3 = print(validate_pin("1.234"))
+test4 = print(validate_pin("00000000"))
+test5 = print(validate_pin("a234"))
