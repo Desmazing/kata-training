e3dc2d7e16bb008d7b12a56d74a4b3948fffaf8b
Desmazing <dkimodera1@gmail.com>
2022-09-15 19:26:47 +0300

    validate pin

--- validate_pin.py
+++ validate_pin.py
@@ -1,5 +1,11 @@
+import string
+
 def validate_pin(pin):
-    pass
+    if len(pin) != 4 and len(pin) != 6: return False
+    for i in pin:
+        if i in string.punctuation or i in string.ascii_letters: return False
+        if i == '\n': return False
+    return True
 
 
 test1 = print(validate_pin('1234'))
@@ -7,3 +13,4 @@ test2 = print(validate_pin("-1234"))
 test3 = print(validate_pin("1.234"))
 test4 = print(validate_pin("00000000"))
 test5 = print(validate_pin("a234"))
+test6 = print(validate_pin("124234"))
