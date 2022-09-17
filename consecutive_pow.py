a7dbeae982a7ca1e2452288db581fae76e9b353c
Desmazing <dkimodera1@gmail.com>
2022-09-17 13:51:29 +0300

    consecutive powers

---
+++ consecutive_pow.py
@@ -0,0 +1,15 @@
+# take a number... sum the digits in it raised to the consecutive powers
+
+
+def consecutive_pow(a,b):
+    lst = []
+    for i in range(a, b+1):
+        a = list(str(i))
+        mult = 0
+        for j,k in enumerate(a):
+            mult += int(k) ** (j+1)
+            if mult == i: lst.append(i)
+    return lst
+
+
+test1 = print(consecutive_pow(1,10))
