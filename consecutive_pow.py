cecbea0966480e3f39621a17fad46692ec9ae693
Desmazing <dkimodera1@gmail.com>
2022-09-17 14:07:34 +0300

    sum of consecutive powers

--- consecutive_pow.py
+++ consecutive_pow.py
@@ -1,4 +1,5 @@
-# take a number... sum the digits in it raised to the consecutive powers
+# take a range of numbers... sum the digits in each raised to the consecutive powers
+# return a list
 
 
 def consecutive_pow(a,b):
@@ -12,6 +13,12 @@ def consecutive_pow(a,b):
     return lst
 
 
+
+def consecutive_pow2(a,b):
+    """this is a refactored version"""
+    return [x for x in range(a, b+1) if sum(int(d)**i for i,d in enumerate(str(x), 1)) == x]
+
+
 test1 = print(consecutive_pow(1,10))
 test2 = print(consecutive_pow(1,100))
-test3 = print(consecutive_pow(10,89))
+test3 = print(consecutive_pow2(10,89))
