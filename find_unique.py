99ef4e5a529cbdc9907116476100dc047fe51ffb
Desmazing <dkimodera1@gmail.com>
2022-09-20 14:25:45 +0300

    find unique in array

---
+++ find_unique.py
@@ -0,0 +1,13 @@
+# find the unique number in an array e.g. [1,2,1,1,1,1] should return 2
+# this function is meant for performance efficiency due to large arrays
+
+
+def unique(n):
+    """this version sorts an array. If the first two elements are alike, return the last
+    else return the first"""
+    a = sorted(n)
+    if a[0]==a[1]: return a[-1]
+    return a[0]
+
+
+test1 = print(unique([1,2,1,1,1,1])) # should return 2
