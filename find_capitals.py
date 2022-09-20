e85f6ad165df8a88c29b98904784f12eab1763d3
Desmazing <dkimodera1@gmail.com>
2022-09-20 15:38:59 +0300

    find uppercase letters in string

---
+++ find_capitals.py
@@ -0,0 +1,14 @@
+# find the indexes of all the uppercase letters
+
+
+def capitals(word):
+    lst = []
+    for i in range(len(word)):
+        if word[i].isupper():
+            lst.append(word.index(word[i], i))
+    return lst
+
+
+
+test1 = print(capitals('SoshdPeqVSqntphhcKYsDmUnHtMuJ'))
+
