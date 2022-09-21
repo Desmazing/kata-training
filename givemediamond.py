9e03a1d7e98ab09423a897c6adda28d6de2a7eb4
Desmazing <dkimodera1@gmail.com>
2022-09-21 10:59:46 +0300

    give me a diamond

--- givemediamond.py
+++ givemediamond.py
@@ -4,11 +4,18 @@
 def diamond(n):
     if n <=0 or n%2==0: return None
     elif n==1: return '*\n'
+    elif n ==3: return " *\n***\n *\n"
+    lst = []
+    for i in range(1,n+1,2):
+        x = (i*'*')
+        lst.append(x.center(n).rstrip())
+    lst = lst + lst[1::-1]
+    return '\n'.join(lst)
 
 
-test0 = print(diamond(0))
+# test0 = print(diamond(0))
 test1 = print(diamond(1))
-test2 = print(diamond(2))
+# test2 = print(diamond(2))
 test3 = print(diamond(3))
-test4 = print(diamond(4))
+# test4 = print(diamond(4))
 test5 = print(diamond(5))
