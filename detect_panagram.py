c96b63d7020c6c8045a570089ce7538e2a8aabf5
Desmazing <dkimodera1@gmail.com>
2022-09-21 16:31:47 +0300

    detect panagrams

--- detect_panagram.py
+++ detect_panagram.py
@@ -5,7 +5,6 @@ import string
 
 
 def is_panagram(s):
-    """check if letters occur to a total of 26. Numbers don't matter"""
     s = s.translate(str.maketrans('','',string.punctuation)).replace(' ','').lower()
     for i in string.ascii_lowercase:
         if i not in s:
@@ -13,6 +12,10 @@ def is_panagram(s):
     return True
 
 
+def is_panagram2(s):
+    return set(string.ascii_lowercase).issubset(s.lower())
+
+
 test1 = print(is_panagram("The quick, brown fox jumps over the lazy dog!"))
-test2 = print(is_panagram("1bcdefghijklmnopqrstuvwxyz"))
-test3 = print(is_panagram("8abcdefgh234ijklmnopqrs56765tuvwxyz1234"))
+test2 = print(is_panagram2("1bcdefghijklmnopqrstuvwxyz"))
+test3 = print(is_panagram2("8abcdefgh234ijklmnopqrs56765tuvwxyz1234"))
