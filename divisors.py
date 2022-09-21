95d4d97882a108bb7986c12974e16c8cce72a9c9
Desmazing <dkimodera1@gmail.com>
2022-09-21 14:40:56 +0300

    find divisors with lambda function

--- divisors.py
+++ divisors.py
@@ -2,6 +2,9 @@ def divisors(n):
     return len([i for i in range(1,n+1) if n%i==0])
 
 
+divisors2 = lambda n: sum([n%i == 0 for i in range(1,n+1)])
+
+
 test1 = print(divisors(1))
 test2 = print(divisors(5))
-test3 = print(divisors(10))
+test3 = print(divisors2(10))
