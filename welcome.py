77b4ec01c199e3065e84b087eeee195d9cb3cf7a
Desmazing <dkimodera1@gmail.com>
2022-09-22 13:24:40 +0300

    welcome message

--- welcome.py
+++ welcome.py
@@ -1,28 +1,18 @@
 #  greet a website user in their language
 
-# db 
+db = {'english': 'Welcome','czech': 'Vitejte','danish': 'Velkomst',
+        'dutch': 'Welkom','estonian': 'Tere tulemast','finnish': 'Tervetuloa',
+        'flemish': 'Welgekomen','french': 'Bienvenue', 'german': 'Willkommen',
+        'irish': 'Failte', 'italian': 'Benvenuto', 'latvian': 'Gaidits',
+        'lithuanian': 'Laukiamas','polish': 'Witamy', 'spanish': 'Bienvenido',
+        'swedish': 'Valkommen', 'welsh': 'Croeso'}
 
 def greet(language):
-    db = {'english': 'Welcome',
-        'czech': 'Vitejte',
-        'danish': 'Velkomst',
-        'dutch': 'Welkom',
-        'estonian': 'Tere tulemast',
-        'finnish': 'Tervetuloa',
-        'flemish': 'Welgekomen',
-        'french': 'Bienvenue',
-        'german': 'Willkommen',
-        'irish': 'Failte',
-        'italian': 'Benvenuto',
-        'latvian': 'Gaidits',
-        'lithuanian': 'Laukiamas',
-        'polish': 'Witamy',
-        'spanish': 'Bienvenido',
-        'swedish': 'Valkommen',
-        'welsh': 'Croeso'}
+    
     try:
         return db[language]
     except:
         return db['english']
 
 test1 = print(greet('english'))
+test2 = print(greet('french'))
