9919efb5282a163a978f0acf6bbb833c1c623c84
Desmazing <dkimodera1@gmail.com>
2022-09-22 13:22:33 +0300

    welcome message

---
+++ welcome.py
@@ -0,0 +1,28 @@
+#  greet a website user in their language
+
+# db 
+
+def greet(language):
+    db = {'english': 'Welcome',
+        'czech': 'Vitejte',
+        'danish': 'Velkomst',
+        'dutch': 'Welkom',
+        'estonian': 'Tere tulemast',
+        'finnish': 'Tervetuloa',
+        'flemish': 'Welgekomen',
+        'french': 'Bienvenue',
+        'german': 'Willkommen',
+        'irish': 'Failte',
+        'italian': 'Benvenuto',
+        'latvian': 'Gaidits',
+        'lithuanian': 'Laukiamas',
+        'polish': 'Witamy',
+        'spanish': 'Bienvenido',
+        'swedish': 'Valkommen',
+        'welsh': 'Croeso'}
+    try:
+        return db[language]
+    except:
+        return db['english']
+
+test1 = print(greet('english'))
