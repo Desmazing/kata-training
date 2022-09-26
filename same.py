08f6c24ba0ac9d7c4a6e66d576248706d77d3499
Desmazing <dkimodera1@gmail.com>
2022-09-26 15:22:05 +0300

    comparing arrays for similarity

--- same.py
+++ same.py
@@ -9,6 +9,15 @@ def comp(array1, array2):
     except:
         return False
 
+def comp2(array1, array2):
+    try:
+        return sorted([i**2 for i in array1]) == sorted(array2)
+    except:
+        return False
+
 
 test1 = print(comp([121, 144, 19, 161, 19, 144, 19, 11], [121, 14641, 20736, 361, 25921, 361, 20736, 361]))
 test2 = print(comp([121, 144, 19, 161, 19, 144, 19, 11], [121, 14641, 20736, 36100, 25921, 361, 20736, 361]))
+print('-----')
+test3 = print(comp2([121, 144, 19, 161, 19, 144, 19, 11], [121, 14641, 20736, 361, 25921, 361, 20736, 361]))
+test4 = print(comp2([121, 144, 19, 161, 19, 144, 19, 11], [121, 14641, 20736, 36100, 25921, 361, 20736, 361]))
