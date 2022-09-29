5b9c178c55bcf115c7ed6cf90f7f4e3455cbdf77
Desmazing <dkimodera1@gmail.com>
2022-09-29 11:24:05 +0300

    divisibility rule of 13

--- divisibility_rule.py
+++ divisibility_rule.py
@@ -1,13 +1,26 @@
+lst = [1,10,9,12,3,4]
+
+
 def thirt(n):
-    lst = [1,10,9,12,3,4] * 4
-    p = str(n)[::-1]
+    """still pending final solution"""
+    strn = str(n)[::-1]
     output = 0
-    while output != int(p[::-1]):
-        for i in range(len(p)):
-            output += int(p[i]) * lst[i]
-        p = str(output)[::-1]
-    return output
+    for i in range(len(strn)):
+        output += int(strn[i]) * lst[i]
+    if n == output:
+        return output
+    else: 
+        thirt(output)
+    
+
+def thirt2(n):
+    """still pending final solution"""
+    strn = str(n)[::-1]
+    m = sum(lst[i % 6] * int(strn[i]) for i in range(len(strn)))
+    return n if n == m else thirt(m)
 
 
-test1 = print(thirt(8529))
-test2 = print(thirt(5634))
+test1 = print(thirt2(8529))
+test2 = print(thirt2(5634))
+test3 = print(thirt2(111111111))
+test4 = print(thirt2(987654321))
