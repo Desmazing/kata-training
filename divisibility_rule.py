b70fff975fbd51017f9bac93d7bc766e32f04e37
Desmazing <dkimodera1@gmail.com>
2022-09-29 11:29:11 +0300

    recursion and divisibility by 13

--- divisibility_rule.py
+++ divisibility_rule.py
@@ -2,25 +2,26 @@ lst = [1,10,9,12,3,4]
 
 
 def thirt(n):
-    """still pending final solution"""
+    """complete solution
+    need to acquaint with recursion a lot more"""
     strn = str(n)[::-1]
     output = 0
     for i in range(len(strn)):
-        output += int(strn[i]) * lst[i]
+        output += int(strn[i]) * lst[i % 6]
     if n == output:
         return output
     else: 
-        thirt(output)
+        return thirt(output)
     
 
 def thirt2(n):
-    """still pending final refactor"""
+    """refactor complete"""
     strn = str(n)[::-1]
     m = sum(lst[i % 6] * int(strn[i]) for i in range(len(strn)))
     return n if n == m else thirt(m)
 
 
-test1 = print(thirt2(8529))
-test2 = print(thirt2(5634))
-test3 = print(thirt2(111111111))
+test1 = print(thirt(8529))
+test2 = print(thirt(5634))
+test3 = print(thirt(1111111111))
 test4 = print(thirt2(987654321))
