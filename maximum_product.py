49bb1d37236318837d83c2751c125bc4da234500
Desmazing <dkimodera1@gmail.com>
2022-09-30 13:14:45 +0300

    max product of adjacent array elements

--- maximum_product.py
+++ maximum_product.py
@@ -11,9 +11,14 @@ def max_product(arr):
     return max(lst)
 
 
+def max_product2(arr):
+    """refactored using zip function solution inspired by one-liner return statements"""
+    return max([a * b for a,b in zip(arr, arr[1:])]) 
+
+
 test1 = print(max_product([5, 8])) # 40
 test2 = print(max_product([1, 2, 3])) # 6
 test3 = print(max_product([1, 5, 10, 9])) # 90
 test4 = print(max_product([5, 1, 2, 3, 1, 4])) # 6
-test5 = print(max_product([-23, 4, -5, 99, -27, 329, -2, 7, -921])) # -14
-test6 = print(max_product([5, 1, 1, 8])) # 8
+test5 = print(max_product2([-23, 4, -5, 99, -27, 329, -2, 7, -921])) # -14
+test6 = print(max_product2([5, 1, 1, 8])) # 8
