099763736f184bd90bad6780a88b5209eb57062b
Desmazing <dkimodera1@gmail.com>
2022-09-30 13:04:58 +0300

    max product of adjacent array elements

---
+++ maximum_product.py
@@ -0,0 +1,18 @@
+#  find the maximum product of adjacent elements in an araay
+
+
+def max_product(arr):
+    lst = []
+    for i in range(len(arr)):
+        for j in range(i+1,len(arr)):
+            if arr[i] == arr[j-1]:
+                lst.append(arr[i] * arr[j])
+                break
+    return max(lst)
+
+
+test1 = print(max_product([5, 8])) # 40
+test1 = print(max_product([1, 2, 3])) # 6
+test1 = print(max_product([1, 5, 10, 9])) # 90
+test1 = print(max_product([5, 1, 2, 3, 1, 4])) # 6
+test1 = print(max_product([-23, 4, -5, 99, -27, 329, -2, 7, -921])) # -14
