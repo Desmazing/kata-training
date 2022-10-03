b4e4c9c7b5b7050a36d0c05f30f3e5f65245ba71
Desmazing <dkimodera1@gmail.com>
2022-10-03 17:56:21 +0300

    string incrementor solution

--- regex_user.py
+++ regex_user.py
@@ -4,9 +4,10 @@ import re
 def validate_user(username):
     """validate username using regex
     use match method where: the username contains 4-16 characters in [a-z0-9_]"""
-    return bool(re.match("^[a-z0-9_]{4,16}$",username)) != None
+    return bool(re.match("^[a-z0-9_]{7,}$",username)) != None
 
 
 test1 = print(validate_user("hallo2"))
-test2 = print(validate_user('1234'))
+test2 = print(validate_user('1294'))
 test3 = print(validate_user('nic'))
+test4 = print(validate_user('*^'))
---
+++ string_incrementor.py
@@ -0,0 +1,27 @@
+"""
+    for a string s:
+    if s ends with a digit eg. 'foo1', return a string with the digit incremented by 1 eg. 'foo2'
+    if not i.e. 'foo', return s concatenated with '1' i.e. 'foo1' 
+"""
+
+
+def increment_string(s):
+    if s == '': return ''
+    str_part = ''
+    digit_part = ''
+    for i in s:
+        if i.isdigit(): digit_part+= i
+        else: str_part += i
+    try:
+        digit_part = str(int(digit_part) + 1).zfill(len(digit_part))
+        return str_part + digit_part
+    except ValueError:
+        return s + '1'
+
+
+test1 = print(increment_string('foo')) # should return 'foo1'
+test2 = print(increment_string('')) # should return ''
+test3 = print(increment_string('foo24')) # should return 'foo25'
+test4 = print(increment_string('hello234')) # should return 'hello235'
+test5 = print(increment_string('mane001')) # should return mane002
+test6 = print(increment_string('foo99')) # should return foo100
