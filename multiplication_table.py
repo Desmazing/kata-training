db4a00a0e0a16baba30195ceea065ce719c0d0eb
Desmazing <dkimodera1@gmail.com>
2022-10-10 16:23:35 +0300

    custom multiplication table

---
+++ multiplication_table.py
@@ -0,0 +1,24 @@
+"""
+    Return a multiplication table given a limit n
+    For instance: if n == 3; return [[1,2,3], [2,4,6],[3,6,9]]
+"""
+
+
+def multable(n):
+    """function to create a custom multiplication table
+    Args:
+        n(int) a limit to the multiplication table
+    Returns:
+        list of lists
+    """
+
+    lst = []
+    for i in range(1, n+1):
+        x = []
+        for j in range(1, n+1):
+            x.append(i*j)
+        lst.append(x)
+    return lst
+
+
+test1 = print(multable(3))
