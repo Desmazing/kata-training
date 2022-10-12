df44f36e9e880fcd616b81552e63815d46b2e34a
Desmazing <dkimodera1@gmail.com>
2022-10-12 09:40:13 +0300

    multipad text writing

---
+++ multipad_text.py
@@ -0,0 +1,23 @@
+"""
+    Determine how many key presses a user has to do to write a phrase
+"""
+
+
+def presses(phrase):
+    """function to determine number of presses a user does to write a phrase
+        on an old mobile phone
+
+    Args:       str - phrase to be typed out.
+    Returns:    int - no. of presses undertaken.
+    """
+
+    press_dict = {'A':1,'B':2,'C':3,'D':1,'E':2,'F':3,'G':1,'H':2,'I':3,
+                 'J':1,'K':2,'L':3,'M':1,'N':2,'O':3,'P':1,'Q':2,
+                 'R':3,'S':4,'T':1,'U':2,'V':3,'W':1,'X':2,'Y':3,'Z':4, ' ':1,
+                 '0':2,'1':1,'2':4,'3':4,'4':4,'5':4,'6':4,'7':5,'8':4,'9':5,
+                 '*':1,'#':1}
+    
+    return f"{sum(press_dict[i.upper()] for i in phrase)} presses"
+
+
+test1 = print(presses('HOW R U')) # should return 13 presses
