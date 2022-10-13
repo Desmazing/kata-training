c76cbbf959475d7750ca0076e544556ac3902ecd
Desmond O <100370067+Desmazing@users.noreply.github.com>
2022-10-13 22:44:48 +0300

    classes and instances

--- ball_type.py
+++ ball_type.py
@@ -1,4 +1,4 @@
-# create a class
+# create a class. Basic intro to classes and instances in Python
 # when an object of the class is instantiated, you can assign a ball type or leave it at default
 
 
