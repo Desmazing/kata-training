20fe82de78c59841d3b7159272bcb643d9402922
Desmazing <dkimodera1@gmail.com>
2022-10-14 23:07:10 +0300

    strong number

---
+++ strong_number.py
@@ -0,0 +1,9 @@
+"""
+    a strong number is one whose sum of factorial of individual digits is equal to the number
+    e.g.
+        145 is strong because:
+            1! + 4! + 5! = 1 + 24 + 120 ~ 145
+        
+        7 is not strong because:
+            7! = 5040 which is not equal to 7
+"""
