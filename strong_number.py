503e71bd44e3a11eece322f4ddd081840caa0477
Desmazing <dkimodera1@gmail.com>
2022-10-14 23:13:05 +0300

    strong number

--- strong_number.py
+++ strong_number.py
@@ -8,9 +8,20 @@
             7! = 5040 which is not equal to 7
 """
 
+import math
 
 def strong_number(num):
     """Function to determine a strong number
     Args:       num (int)   positive integer
     Returns:    str : 'Strong!' or not 'Not Strong!'
     """
+
+    total = 0
+    for i in str(num):
+        total += math.factorial(int(i))
+    return "Strong!" if num == total else "Not Strong!"
+
+
+test1 = print(strong_number(145))   # should return "Strong!"
+test2 = print(strong_number(7))     # should return "Not Strong!"
+test3 = print(strong_number(9))     # should return "Not Strong!"
