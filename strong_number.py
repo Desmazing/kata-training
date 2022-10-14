c9ff75b55a7d94b7a94ead4142628daa51ef99ff
Desmazing <dkimodera1@gmail.com>
2022-10-14 23:17:39 +0300

    strong number

--- strong_number.py
+++ strong_number.py
@@ -22,6 +22,26 @@ def strong_number(num):
     return "Strong!" if num == total else "Not Strong!"
 
 
+def strong2_fact(n):
+    """Function to create a factorial without using the math module
+    Args: n (int)
+    Returns: int: the factorial of n
+    """
+
+    return math.prod(list(range(1, n + 1)))
+
+
+def strong_number2(num):
+    """Function to determine whether a number is strong or not
+    Args: num (int): positive integer 
+    Returns:    str : 'Strong!' or not 'Not Strong!'
+    """
+    total = 0
+    for i in str(num):
+        total += strong2_fact(int(i))
+
+
+
 test1 = print(strong_number(145))   # should return "Strong!"
 test2 = print(strong_number(7))     # should return "Not Strong!"
 test3 = print(strong_number(9))     # should return "Not Strong!"
