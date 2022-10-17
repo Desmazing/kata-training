acc8888fec88bdfe21ea0ff6d8561ebb05414e18
Desmazing <dkimodera1@gmail.com>
2022-10-17 22:44:22 +0300

    consonant_value

---
+++ consonant_value.py
@@ -0,0 +1,21 @@
+def solve(s):
+    """Function to find the value of the longest consonant substring
+    Args:       s (string)
+    Returns:    int (consonant value)
+    """
+    max_ord = 0
+    curr_ord = 0
+    
+    for i in s:
+        if i not in 'aeiou':
+            curr_ord += ord(i) - 96
+        else:
+            if curr_ord > max_ord:
+                max_ord = curr_ord
+            curr_ord = 0
+    return max_ord
+
+
+test1 = print(solve('zodiac'))
+test2 = print(solve('strength'))
+test3 = print(solve('catchphrase'))
