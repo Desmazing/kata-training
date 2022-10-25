524576ca2c090d8c6361c919a97ad62b998934a3
Desmazing <dkimodera1@gmail.com>
2022-10-25 11:05:56 +0300

    findall() regex module

---
+++ order.py
@@ -0,0 +1,23 @@
+"""
+    Finding the correct sequence of an order from a menu
+"""
+
+
+import re
+
+
+def get_order(order):
+    """function to get order from a menu
+    Args:       order (str)
+    Returns:    str (correct order)
+    """
+    
+    menu_pattern = 'Burger|Fries|Chicken|Pizza|Sandwich|Onionrings|Milkshake|Coke'
+    menu_list = menu_pattern.split('|')
+
+    retrieved_order = re.findall(menu_pattern, order, re.IGNORECASE)
+    
+
+    
+
+print(get_order('milkshakeonionringsburgerfries'))
