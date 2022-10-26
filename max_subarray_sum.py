9ed41323bdc38ee18452f4de2932975af62d4865
Desmazing <dkimodera1@gmail.com>
2022-10-26 13:38:50 +0300

    maximum sum in contiguous array elements

--- max_subarray_sum.py
+++ max_subarray_sum.py
@@ -18,6 +18,22 @@ def max_sequence(arr):
     return max(sum_list)
 
 
+def max_sequence2(arr):
+    """funtion to find the maximum sum for contiguous elements in an array
+
+    Args:       arr (list)  An array of integers
+    Returns:    int - maximum sum
+    """
+    maxl, curr = 0,0
+    for i in arr:
+        curr += i
+        if curr < 0:
+            curr = 0
+        elif curr > maxl:
+            maxl = curr
+    return maxl
+
+
 # the above method is not the most efficient. need to refactor.
 
 
@@ -29,3 +45,6 @@ test3=print(max_sequence([8, 6, 12, 9, 3, 13, -14, 15, -16, 14, 15, 18, 17, 2, 2
 # should return 122
 test4=print(max_sequence([18, -13, 17, 15, 12, -13, 16, -1, 4, 7, 8, -10, 15, -7, -6, 16]))
 # should return 78
+test5=print(max_sequence2([]))
+test6=print(max_sequence2([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
+test6=print(max_sequence2([18, -13, 17, 15, 12, -13, 16, -1, 4, 7, 8, -10, 15, -7, -6, 16]))
