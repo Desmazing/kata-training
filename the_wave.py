bf2178caff5bd111853c1c00c4cbf2948ce95d1c
Desmazing <dkimodera1@gmail.com>
2025-01-15 11:31:12 +0300

    the wave implementation using strings

--- the_wave.py
+++ the_wave.py
@@ -24,5 +24,18 @@ def wave(people):
     return my_list
 
 
+def wave_version_2(people):
+    """
+    Function to simulate The Mexican Wave
+    args:       String(lowercase) > to represent people seated in a stadium
+    returns:    List > to simulate the wave
+    """
+    return [people[:index] + people[index].upper() + people[index+1:] \
+            for index, letter in enumerate(people) if letter.isalpha()]
+
+
 print(repr(wave("hello")))
 print(repr(wave("two words")))
+
+print(repr(wave_version_2("hello")))
+print(repr(wave_version_2("two words")))
